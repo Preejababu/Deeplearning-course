ff1240b655297234890bd2aa323493ab6660c488
preeja prathap <preejaprathap@gmail.com>
2021-09-22 15:31:12 +0530

    first commit

---
+++ and.py
@@ -0,0 +1,30 @@
+from utils.model import  Perceptron 
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import joblib # FOR SAVING MY MODEL AS A BINARY FILE
+from matplotlib.colors import ListedColormap
+from utils.all_utils import prepare_data
+import os
+AND = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y": [0,0,0,1],
+}
+
+df = pd.DataFrame(AND)
+
+df
+
+
+
+X,y = prepare_data(df)
+
+ETA = 0.3 # 0 and 1
+EPOCHS = 10
+
+model = Perceptron(eta=ETA, epochs=EPOCHS)
+model.fit(X, y)
+
+_ = model.total_loss()
